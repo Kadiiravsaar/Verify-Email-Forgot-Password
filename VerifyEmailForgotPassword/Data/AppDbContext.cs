e77b86bd3cc1b3915d8b15a1ff27da45b23ee221
kadiiravsaar <kadiiravsar@gmail.com>
2023-04-10 05:22:27 +0300

    Add project files.

---
+++ VerifyEmailForgotPassword/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Reflection;
+using VerifyEmailForgotPassword.Models;
+
+namespace VerifyEmailForgotPassword.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            // tüm assembly seviyesinde configurationları tarar ve bulur
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<User> Users => Set<User>();
+    }
+
+}
