c1495c7479bd69e972c2607e9cda41319b5380a8
kadiiravsaar <kadiiravsar@gmail.com>
2023-04-20 07:12:21 +0300

    UserLoginRequest Added UserRegisterRequest Added

---
+++ VerifyEmailForgotPassword/Controllers/UserController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
+using System.Security.Cryptography;
+using VerifyEmailForgotPassword.Data;
+using VerifyEmailForgotPassword.Models;
+
+namespace VerifyEmailForgotPassword.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+            if (_context.Users.Any(u => u.Email == request.Email))
+            {
+                return BadRequest("User Already Exists!");
+            }
+
+            CreatePasswordHash(request.Password,
+                out byte[] passwordHash,
+                out byte[] passwordSalt);
+
+            var user = new User()
+            {
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                VerificationToken = CreateRandomToken()
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok($"User Successfully Created (Token: {user.VerificationToken}) ");
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginRequest login)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == login.Email);
+            if (user == null)
+            {
+                return BadRequest("User Not Found");
+            }
+            if (user.VerifiedAt == null)
+            {
+                return BadRequest("Not verified! ");
+            }
+            if (!VerifiedPasswordHash(login.Password,user.PasswordHash,user.PasswordSalt))
+            {
+                return BadRequest("Password is incorrect.");
+            }
+            if (user.VerifiedAt == null)
+            {
+                return BadRequest("Not verified!");
+            }
+
+            return Ok($"Welcome back, {user.Email}! :)");
+        }
+
+
+        [HttpPost("verify")]
+        public async Task<IActionResult> Verify(string tokens)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.VerificationToken == tokens);
+            if (user == null)
+            {
+                return BadRequest("Invalid Token");
+            }
+
+            user.VerifiedAt = DateTime.Now;
+            await _context.SaveChangesAsync();
+            return Ok("User Verified");
+        }
+
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+            }
+        }
+
+        private bool VerifiedPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+               var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+
+            }
+        }
+        private string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+    }
+}
